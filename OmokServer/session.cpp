ba8ee5c5e46d1a2d5dc17189f32a269e02b15150
sykim2312 <sykim2312@com2us.com>
2024-03-06 18:18:47 +0900

    Revert "fix : Session::onShutdown (remove session)"
    This reverts commit ad9a15ca13324c19bfbf6d07b6c2a812032343b6.

--- OmokServer/session.cpp
+++ OmokServer/session.cpp
@@ -40,8 +40,6 @@ void Session::onReadable(ReadableNotification* pNotification)
 void Session::onShutdown(ShutdownNotification* pNotification)
 {
 	pNotification->release();
-	SessionManager session_manager;
-	session_manager.RemoveSession(session_id_);
 }
 
 Session::~Session()
