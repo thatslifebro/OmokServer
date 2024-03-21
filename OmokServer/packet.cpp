acea32dbb794ce830868f959b925b3c7a8a8ac45
sykim2312 <sykim2312@com2us.com>
2024-03-21 11:29:35 +0900

    rename

--- OmokServer/packet.cpp
+++ OmokServer/packet.cpp
@@ -6,17 +6,17 @@ void Packet::ParseData(std::shared_ptr<char[]> buffer)
 
 	memcpy(&packet_header_.GetPacketId(), buffer.get() + sizeof(packet_header_.GetPacketSize()), sizeof(packet_header_.GetPacketId()));
 
-	memcpy(packet_byte_array_, buffer.get(), packet_header_.GetPacketSize());
+	memcpy(byte_array_, buffer.get(), packet_header_.GetPacketSize());
 }
 
 void Packet::SetPacketSize(uint16_t packet_size)
 {
 	packet_header_.GetPacketSize() = packet_size;
-	memcpy(packet_byte_array_, &packet_header_.GetPacketSize(), sizeof(packet_header_.GetPacketSize()));
+	memcpy(byte_array_, &packet_header_.GetPacketSize(), sizeof(packet_header_.GetPacketSize()));
 }
 
 void Packet::SetPacketId(uint16_t packet_id)
 {
 	packet_header_.GetPacketId() = packet_id;
-	memcpy(packet_byte_array_ + sizeof(packet_header_.GetPacketSize()), &packet_header_.GetPacketId(), sizeof(packet_header_.GetPacketId()));
+	memcpy(byte_array_ + sizeof(packet_header_.GetPacketSize()), &packet_header_.GetPacketId(), sizeof(packet_header_.GetPacketId()));
 }
--- OmokServer/packet.h
+++ OmokServer/packet.h
@@ -17,16 +17,16 @@ public:
 	uint16_t GetPacketId() { return packet_header_.GetPacketId(); }
 	void SetPacketId(uint16_t packet_id);
 
-	char* GetPacketBody() { return packet_byte_array_ + PacketHeader::HEADER_SIZE; }
+	char* GetPacketBody() { return byte_array_ + PacketHeader::HEADER_SIZE; }
 	uint16_t GetBodySize() { return packet_header_.GetPacketSize() - PacketHeader::HEADER_SIZE; }
 
 	uint32_t GetSessionId() { return session_id_; }
 	void SetSessionId(uint32_t session_id) { session_id_ = session_id; }
 
-	char* GetPacketByteArray() { return packet_byte_array_; }
+	char* GetPacketByteArray() { return byte_array_; }
 
 private:
 	PacketHeader packet_header_;
 	uint32_t session_id_;
-	char packet_byte_array_[MAX_PACKET_SIZE] = { 0, };
+	char byte_array_[MAX_PACKET_SIZE] = { 0, };
 };
